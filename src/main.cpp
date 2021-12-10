8e42dd4e2002965deef4a8906f22d81be72d9f98
NULLCT <cockatiel.u10@gmail.com>
2021-12-11 01:23:02 +0900

    NEMUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,10 @@
 ﻿#include "dotsandboxes.hpp"
 
-int main() {
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
-  cout << "縦,横を入力" << endl;
-  cin >> N >> M;
-  Support();
+signed main() {
+  Wrapper dotsandboxes;
 
-  /*
-  cout << "今引かれている線の本数を入力" << endl;
-  int x;
-  cin >> x;
-  llint Game = 0;
-  if (x != 0) {
-          cout << "線が引かれている箇所を入力" << endl;
-          rep(i, x) {
-                  int y;
-                  cin >> y;
-                  Game += (llint)1 << y;
-          }
-  }
-  cout << Count(Game) << endl;
-  */
-  return 0;
+  cout << "縦,横を入力" << endl;
+  cin >> dotsandboxes.N >> dotsandboxes.M;
+
+  dotsandboxes.Support();
 }
